d6982a2dff873d402c0e578ff27358ea8dc5f3f5
Saber Modirian <sabermodirian@gmail.com>
2026-01-07 13:41:36 +0330

    نظیمات پنل ادمین
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+'''
+@admin.register(Post)  # <--- این دکوراتور کار همون خط آخر رو انجام میده '''
+
+
+class PostAdmin(admin.ModelAdmin):
+    # اینجا گفتی چی نشون بده
+    list_display = ('title', 'author', 'modified_datetime', 'status')
+    ordering = ['-modified_datetime', 'status', 'title']
+
+    # اینجا گفتی: مدل Post رو با تنظیمات PostAdmin ثبت کن!
+
+
+admin.site.register(Post, PostAdmin)
