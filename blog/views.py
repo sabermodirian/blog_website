1c25b00f7d33714d2bfb4114125d73a7aba11cd8
Saber Modirian <sabermodirian@gmail.com>
2026-01-07 23:51:03 +0330

    64)نمایش لیست پست ها در ی سایت وبلاگ اولین صفحه
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
 # Create your views here.
+def post_list_view(request):
+    posts_lst = Post.objects.all()
+    return render(request, 'blog/posts_list.html', {'postslist': posts_lst})
