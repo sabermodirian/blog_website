1d3d33f107876ec78adfb1f8d18440834e4a4d3a
Saber Modirian <sabermodirian@gmail.com>
2026-01-11 00:27:01 +0330

    76 و 77و75)دریافت ورودی توسط URL به view سپس پیدا کردن  پستلاگ در دیتابیس و نمایش آن در تمپلیت post_detal.html یعنی صفحهی مشخصات و توضیحات مخصوص هر پستلاگ
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list_view, name='postslist')
+    path('', views.post_list_view, name='postslist'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,10 @@ from .models import Post
 def post_list_view(request):
     posts_lst = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'postslist': posts_lst})
+
+
+def post_detail_view(request, pk):
+    # print('Id in URL:',pk)
+    post_log = Post.objects.get(pk=pk)
+    # return HttpResponse(f'ID:{pk}')
+    return render(request, 'blog/post_detail.html', {'pst': post_log})
