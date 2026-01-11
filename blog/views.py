61ee735690660d68b8a7491d5606c5c4790344eb
Saber Modirian <sabermodirian@gmail.com>
2026-01-11 11:59:04 +0330

    78)نمایش دکمه در صفحه ی اصلی برای ارجاع به صفحه ی detail_view
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from .models import Post
 # Create your views here.
 def post_list_view(request):
     posts_lst = Post.objects.all()
-    return render(request, 'blog/posts_list.html', {'postslist': posts_lst})
+    # text = posts_lst.get_queryset(text)
+    return render(request, 'blog/posts_list.html',
+                  {'postslist': posts_lst})
 
 
 def post_detail_view(request, pk):
