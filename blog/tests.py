c48940f4508826bf61486acbf24e7f4c9ac4d2f8
Saber Modirian <sabermodirian@gmail.com>
2026-01-11 22:22:42 +0330

    81)اضافه کردن تست و تست نویسی به پروژه  سایت وبلاگ
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/tests.py
+++ blog/tests.py
@@ -1,1 +1,27 @@
 # Create your tests here.
+# from unittest import TestCase
+
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+from django.test import TestCase
+
+from .models import Post
+
+
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user01')
+        self.post_1 = Post.objects.create(
+            title='post01'
+            , text='this is a text for test of post01',
+            status=Post.STATUS_CHOICES[0],
+            author=self.user
+        )
+
+    def test_PostList_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_PostList_urlByName(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertEqual(response.status_code, 200)
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list_view, name='postslist'),
+    path('', views.post_list_view, name='posts_list'),
     path('<int:pk>/', views.post_detail_view, name='post_detail')
 ]
