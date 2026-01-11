6eebb511133039c39850e402d83a4232ba63ab31
Saber Modirian <sabermodirian@gmail.com>
2026-01-11 20:01:30 +0330

    79,80)مدیریت اشتباه سهوی یا عمدی کاربر در ورود شماره آی دی و استفاده از get_object_or_404
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from .models import Post
@@ -13,6 +14,6 @@ def post_list_view(request):
 
 def post_detail_view(request, pk):
     # print('Id in URL:',pk)
-    post_log = Post.objects.get(pk=pk)
+    post_log = get_object_or_404(Post, pk=pk)
     # return HttpResponse(f'ID:{pk}')
     return render(request, 'blog/post_detail.html', {'pst': post_log})
