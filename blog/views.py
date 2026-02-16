68f8c514590911d87771f72fecdd98da73958b3f
Saber Modirian <sabermodirian@gmail.com>
2026-02-16 20:15:29 +0330

    93)دریافت و ارسال اطلاعات به فرم در ویو و تفاوت آنها در زمان  متد GET و POST
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -21,4 +21,11 @@ def post_detail_view(request, pk):
 
 
 def post_create_view(request):
+    if request.method == "GET":
+        print('This is a GET  request method')
+    elif request.method == "POST":
+        # form data
+        print(f'This is a POST  request method: {request.POST}')
+        print(f'This is Title of a POST  request method :{request.POST.get('title')}')
+        print(f'This is Text of a POST  request method :{request.POST.get('text')}')
     return render(request, 'blog/post_create.html')
