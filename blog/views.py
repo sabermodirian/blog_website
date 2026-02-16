32943b01b294be7bb0363580bb7cf74298f5aa76
Saber Modirian <sabermodirian@gmail.com>
2026-02-16 14:38:31 +0330

    90)ساخت url , view برای create_view
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -19,3 +19,6 @@ def post_detail_view(request, pk):
     # return HttpResponse(f'ID:{pk}')
     return render(request, 'blog/post_detail.html', {'pst': post_log})
 
+
+def post_create_view(request):
+    return render(request, 'blog/post_create.html')
