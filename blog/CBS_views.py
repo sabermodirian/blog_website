a92cce0fd681d05d034b9ffc6365a724f47d54b0
Saber Modirian <sabermodirian@gmail.com>
2026-02-19 18:00:24 +0330

    107)ساخت کلاس ویوی PostDetailCBS_View  و البته مسیر مخصوص( در واقع جداسازی Class Base View و FBS_views  ) آن در urls.py
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- blog/CBS_views.py
+++ blog/CBS_views.py
@@ -5,9 +5,14 @@ from .models import Post
 
 class PostListCBS_View(generic.ListView):
     model = Post
-
     def get_queryset(self):
         return Post.objects.filter(status='publsh').order_by('-modified_datetime')
 
     template_name = 'blog/posts_list.html'
     context_object_name = 'postslist'
+
+
+class PostDetailCBS_View(generic.DetailView):  # خودش دنبال pk میگرده
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'pst'
