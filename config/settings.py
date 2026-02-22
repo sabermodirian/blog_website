ed98115cce97f225ee7dab4a9b74d860255b3e31
Saber Modirian <sabermodirian@gmail.com>
2026-02-22 18:20:47 +0330

    111)ساختن  اپ accounts و لاگین برای سایت وبلاگ با استفاده از اپ درونی auth و بدون ایجاد عجیب و قریب view وurls برای اپ accounts
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'blog:posts_list'
