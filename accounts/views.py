75e6060acb3e695b98d197c7e99527f375533152
Saber Modirian <sabermodirian@gmail.com>
2026-02-23 08:08:46 +0330

    117 ) ساخت صفحه ی ثبت نام در سایت (SignUp.html)
    Signed-off-by: Saber Modirian <sabermodirian@gmail.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
